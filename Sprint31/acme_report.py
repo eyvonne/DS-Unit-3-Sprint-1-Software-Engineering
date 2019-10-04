77e7b5388388cdab0b9c485774512a3ef9b4c38b
eyvonne <35879510+eyvonne@users.noreply.github.com>
2019-10-04 10:38:43 -0700

    sprint Challenge Update

--- Sprint31/acme_report.py
+++ Sprint31/acme_report.py
@@ -1,12 +1,12 @@
 from random import randint, sample, uniform
-from acme import Product
+from acme import Product, BoxingGlove
 
 # Useful to use with random.sample to generate names
 ADJECTIVES = ['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved']
 NOUNS = ['Anvil', 'Catapult', 'Disguise', 'Mousetrap', '???']
 
 
-def generate_products(num_products=30):
+def generate_products(num_products=30, producttype=Product):
     """generates a list of generic products with names constructed from given
     lists. Generates number of products passed in, default 30
     """
@@ -15,7 +15,7 @@ def generate_products(num_products=30):
         adj = sample(ADJECTIVES, 1)[0]
         noun = sample(NOUNS, 1)[0]
         name = adj+' '+noun
-        prod = Product(name)
+        prod = producttype(name)
         prod.weight = randint(5, 100)
         prod.price = randint(5, 100)
         prod.flammability = uniform(0, 2.5)
@@ -35,6 +35,13 @@ def inventory_report(products):
     print(f'Average weight: {meanWeight}')
     meanFlammability = (sum([i.flammability for i in products])/len(products))
     print(f'Average flammability: {meanFlammability}')
+    steal = sorted([i.stealability() for i in products])
+    kinda = sum([1 if i == 'Kinda stealable' else 0 for i in steal])
+    nots = sum([1 if i == 'Not so stealable...' else 0 for i in steal])
+    very = sum([1 if i == 'Very stealable' else 0 for i in steal])
+    print('Count of Not so stealable products: ', nots)
+    print('Count of Kinda Stealable products: ', kinda)
+    print('Count of Very stealable products: ', very)
 
 
 if __name__ == '__main__':
